cde64678451f26552637d95006119ea49ee0d66e
bhamburg <bhamburg@gmail.com>
2020-05-26 00:16:37 -0400

    created Latest Posts widget and init blog section

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import { Container, Row, Button, Col, Card, ButtonGroup } from "react-bootstrap"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Header from "../components/header"
+import Dump from "../components/Dump"
 
 const IndexPage = () => {
   const data = useStaticQuery(graphql`
@@ -14,6 +15,20 @@ const IndexPage = () => {
         childImageSharp {
           sizes(quality: 100) {
             ...GatsbyImageSharpSizes_withWebp
+          }
+        }
+      }
+      allMdx(
+        sort: { fields: [frontmatter___date], order: DESC }
+        filter: { frontmatter: { published: { eq: true } } }
+      ) {
+        nodes {
+          id
+          excerpt(pruneLength: 250)
+          frontmatter {
+            title
+            date
+            description
           }
         }
       }
@@ -31,101 +46,56 @@ const IndexPage = () => {
       />
       <main>
         <section className="bg-white" id="spotlight">
-          <Container>
-            <h2 className="mb-4">Latest Posts</h2>
+        <Container>
+            <h2>Featured Project</h2>
             <Row>
-              <Col md={3}>
-                <Card className="mb-4 shadow-sm">
-                  <Card.Img variant="top" src="holder.js/100px180" />
-                  <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
-                    <Card.Text>
-                      Some quick example text to build on the card title and make up the bulk of
-                      the card's content.
-                    </Card.Text>
-                    <div className="d-flex justify-content-between align-items-center">
-                        <ButtonGroup>
-                          <Button variant="outline-secondary" size="sm">View</Button>
-                          <Button variant="outline-secondary" size="sm">Edit</Button>
-                        </ButtonGroup>
-                      <small className="text-muted">9 mins</small>
-                    </div>
-                  </Card.Body>
-                </Card>
-              </Col>
-              <Col md={3}>
-                <Card className="mb-4 shadow-sm">
-                  <Card.Img variant="top" src="holder.js/100px180" />
-                  <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
-                    <Card.Text>
-                      Some quick example text to build on the card title and make up the bulk of
-                      the card's content.
-                    </Card.Text>
-                    <div className="d-flex justify-content-between align-items-center">
-                        <ButtonGroup>
-                          <Button variant="outline-secondary" size="sm">View</Button>
-                          <Button variant="outline-secondary" size="sm">Edit</Button>
-                        </ButtonGroup>
-                      <small className="text-muted">9 mins</small>
-                    </div>
-                  </Card.Body>
-                </Card>
-              </Col>
-              <Col md={3}>
-                <Card className="mb-4 shadow-sm">
-                  <Card.Img variant="top" src="holder.js/100px180" />
-                  <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
-                    <Card.Text>
-                      Some quick example text to build on the card title and make up the bulk of
-                      the card's content.
-                    </Card.Text>
-                    <div className="d-flex justify-content-between align-items-center">
-                        <ButtonGroup>
-                          <Button variant="outline-secondary" size="sm">View</Button>
-                          <Button variant="outline-secondary" size="sm">Edit</Button>
-                        </ButtonGroup>
-                      <small className="text-muted">9 mins</small>
-                    </div>
-                  </Card.Body>
-                </Card>
-              </Col>
-              <Col md={3}>
-                <Button className="display-1">
-                  &rarr;
-                </Button>
+              <Col>
+                <img src="https://source.unsplash.com/KE0nC8-58MQ/660x360" alt={`placeholder`} />
               </Col>
             </Row>
           </Container>
         </section>
         <section className="bg-light" id="work">
-          <Container>
-            <h2>Work</h2>
-            <Row>
-              <Col>
-              I am work.<br/>
-              <Card className="mb-4 shadow-sm">
-                <Card.Body>
-                  <Card.Title>{`<code>`}</Card.Title>
-                  <Card.Text>
-                    For example, <code>&lt;section&gt;</code> should be wrapped as inline.
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-              <Card className="shadow-sm bg-dark">
-                <Card.Body>
-                  <pre className="text-light">
-                    <code>
-                      &lt;p&gt;Sample text here...&lt;/p&gt;
-                      &lt;p&gt;And another line of sample text here...&lt;/p&gt;
-                    </code>
-                  </pre>
-                </Card.Body>
-              </Card>
-              </Col>
-            </Row>
-          </Container>
+        <Container>
+          <h2 className="mb-4">Latest Posts</h2>
+          {/* <Dump data={data} /> */}
+          <Row>
+            {data.allMdx.nodes.map(({ excerpt, frontmatter }, index) => {
+              if (index == 0) return (
+                <Col md={6} xl={4} className="mb-5">
+                  <Card as="a" className="shadow-sm h-100" href="#!">
+                    <Card.Img variant="top" src="https://source.unsplash.com/KE0nC8-58MQ/660x360" alt={frontmatter.title} />
+                    <Card.Body>
+                      <Card.Title>{frontmatter.title}</Card.Title>
+                      <Card.Text>{frontmatter.description || excerpt}</Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                      {frontmatter.date}
+                    </Card.Footer>
+                  </Card>
+                </Col>
+              )}
+            )}
+          </Row>
+          <Row>
+            {data.allMdx.nodes.map(({ excerpt, frontmatter }, index) => {
+              if (0 < index && index < 4) return (
+                <Col md={6} xl={4} className="mb-5">
+                  <Card as="a" className="shadow-sm h-100" href="#!">
+                    <Card.Img variant="top" src="https://source.unsplash.com/KE0nC8-58MQ/660x360" alt={frontmatter.title} />
+                    <Card.Body>
+                      <Card.Title>{frontmatter.title}</Card.Title>
+                      <Card.Text>{frontmatter.description || excerpt}</Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                      {frontmatter.date}
+                    </Card.Footer>
+                  </Card>
+                </Col>
+              )}
+            )}
+          </Row>
+        </Container>
         </section>
         <section className="bg-warning" id="hobbies">
           <Container>
@@ -133,16 +103,6 @@ const IndexPage = () => {
             <Row>
               <Col>
                 I am hobbies.
-              </Col>
-            </Row>
-          </Container>
-        </section>
-        <section className="bg-light" id="latest_posts">
-          <Container>
-            <h2>Latest Posts</h2>
-            <Row>
-              <Col>
-                I am a post.
               </Col>
             </Row>
           </Container>
