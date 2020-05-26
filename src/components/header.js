e3c42ae4acbcda76230651697d4c347e75c6547f
bhamburg <bhamburg@gmail.com>
2020-05-26 12:38:18 -0400

    resolved merge conflict

--- src/components/header.js
+++ src/components/header.js
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
-
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
-=======
 import PropTypes from "prop-types"
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
@@ -83,7 +49,6 @@ const Header = ({ title, subtitle, text, img }) => {
     </BackgroundImage>
   )
 }
->>>>>>> feat/nav
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
